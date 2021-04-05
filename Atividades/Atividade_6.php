dcbbdff6d69a7eb610eba091868f8a69571137f5
Jonathan Meira <jonathan_castilho_meira@hotmail.com>
2021-04-05 20:29:01 -0300

    Correção de erros da sexta atividade

--- Atividades/Atividade_6.php
+++ Atividades/Atividade_6.php
@@ -2,6 +2,7 @@
 
 $randomNumbers = [];
 $userInput = null;
+$numberOfTimes = null;
 
 for($i=0;$i<30;$i++){
 	$randomNumbers[$i] = mt_rand(1,15);
@@ -18,5 +19,12 @@ foreach($randomNumbers as $number =>$value)
     if($randomNumbers[$number] == $userInput)
     {
         echo "Your's number  is present at array position: $number \n";
+        $numberOfTimes++;
     }
+}
+
+if(!$numberOfTimes){
+    echo "Your number wasn't generated at all";
+}else{
+    echo "You number has generated $numberOfTimes times";    
 }
